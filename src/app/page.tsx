3df957be28e121d032f4ac9d75c88714b37efa7a
Juan Pablo Reyes Herrera <juanpabloreyesherrera@gmail.com>
2026-06-23 12:18:18 -0500

    chore: home active

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,31 +6,30 @@ import { Overlay } from "@/components/shared/overlay";
 
 export default function Home() {
   return (
-    // <div className="w-full font-sans flex flex-col items-center justify-center bg-linear-to-b from-foreground via-foreground to-background">
-    //   {/** ARREGLAR ESTE PARCHE */}
-    //   <AddSocialMedia />
-    //   <section
-    //     id="home"
-    //     className="relative h-[130dvh] w-full md:max-w-[80dvw] xl:max-w-[60dvw]"
-    //   >
-    //     <BackGroundImage imageSrc="/assets/hero.jpg" alt="Hero" />
-    //     <HeroSection youtubeVideos={MOCK_YOUTUBE_VIDEOS} />
-    //   </section>
+    <div className="w-full font-sans flex flex-col items-center justify-center bg-linear-to-b from-foreground via-foreground to-background">
+      {/** ARREGLAR ESTE PARCHE */}
+      <AddSocialMedia />
+      <section
+        id="home"
+        className="relative h-[130dvh] w-full md:max-w-[80dvw] xl:max-w-[60dvw]"
+      >
+        <BackGroundImage imageSrc="/assets/hero.jpg" alt="Hero" />
+        <HeroSection youtubeVideos={MOCK_YOUTUBE_VIDEOS} />
+      </section>
 
-    //   <section
-    //     id="Spotify"
-    //     className="relative h-[120dvh] w-full md:max-w-[80dvw] xl:max-w-[60dvw]"
-    //   >
-    //     <BackGroundImage imageSrc="/assets/spotify-section.jpg" alt="spotify" />
-    //     <SpotifySection spotifyUrl={MOCK_SPOTIFY_TRACK} />
-    //   </section>
-    //   <section
-    //     id="beats"
-    //     className="w-full h-[150dvh] flex justify-center md:max-w-[80dvw] xl:max-w-[60dvw]"
-    //   >
-    //     <BeatsLanding />
-    //   </section>
-    // </div>
-    <div>hola</div>
+      <section
+        id="Spotify"
+        className="relative h-[120dvh] w-full md:max-w-[80dvw] xl:max-w-[60dvw]"
+      >
+        <BackGroundImage imageSrc="/assets/spotify-section.jpg" alt="spotify" />
+        <SpotifySection spotifyUrl={MOCK_SPOTIFY_TRACK} />
+      </section>
+      <section
+        id="beats"
+        className="w-full h-[150dvh] flex justify-center md:max-w-[80dvw] xl:max-w-[60dvw]"
+      >
+        <BeatsLanding />
+      </section>
+    </div>
   );
 }
